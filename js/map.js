6d457f97d5cb2228519748691d89e29f277b8fa8
SobolevVova <sobole.vova1978@gmail.com>
2018-06-26 12:57:22 +0300

    Домашка

--- js/map.js
+++ js/map.js
@@ -113,50 +113,34 @@ var getPins = function (pins) {
 mapPins.appendChild(getPins(posters));
 
 // Задание 5
+var mapCard = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
+var map = document.querySelector('.map');
+var filterContainer = document.querySelector('.map__filters-container');
+
+var getTitleType = function (type) {
+  switch (type) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalo':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец ';
+  }
+  return 'Вам повезёт';
+};
 
 var getMapCard = function (obj) {
-  var mapCard = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
 
   mapCard.querySelector('.popup__title').innerText = obj.offer.title;
   mapCard.querySelector('.popup__text--address').innerText = obj.offer.address;
-  mapCard.querySelector('.popup__text--address').innerHTML = obj.offer.price + '₽/ночь';
-  mapCard.querySelector('.popup__type').innerText = obj.offer.type;
-  mapCard.querySelector('.popup__text--capacity').innerText = obj.offer.rooms + 'комнаты для' + obj.offer.guests + 'гостей';
-  mapCard.querySelector('.popup__text--time').innerText = 'Заезд после' + obj.offer.checkin + ',' + 'выезд до' + obj.offer.checkout;
-
-  var fragment = document.createDocumentFragment();
-
-  for (var j = 0; j < obj.length; j++) {
-    fragment.appendChild(getMapCard(obj[j]));
-  }
-
-
-  var getFeature = function (arr) {
-    mapCard.querySelector('.popup__features').innerHTML = '';
-
-    for (var j = 0; j < arr.length; j++) {
-      mapCard.querySelector('.popup__features').innerText = ('li.popup__feature--' + LIST_FEATURES[j]);
-    }
-
-  };
-
-  mapCard.querySelector('.popup__description').innerText = 'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.' + obj.offer.description;
-
-  var getPhoto = function () {
-    mapCard.querySelector('.popup__photos').innerHTML = '';
-
-    mapCard.querySelector('.popup__photos').innerText = obj.offer.photos;
-    var imgPhoto = mapCard.querySelector('.popup__photos');
-    imgPhoto.classList.add('.popup__photo');
-    imgPhoto.src = 'http://o0.github.io/assets/images/tokyo/hotel2.jpg';
-    imgPhoto.src = 'http://o0.github.io/assets/images/tokyo/hotel3.jpg';
-    imgPhoto.src = 'http://o0.github.io/assets/images/tokyo/hotel1.jpg';
-    imgPhoto.style.width = '45' + 'px';
-    imgPhoto.style.height = '40' + 'px';
-    imgPhoto.alt = 'Уютное гнездышко для молодоженов';
-  };
+  mapCard.querySelector('.popup__text--price').innerHTML = obj.offer.price + '₽/ночь';
+  mapCard.querySelector('.popup__type').innerText = getTitleType(obj.offer.type);
+  mapCard.querySelector('.popup__text--capacity').innerText = obj.offer.rooms + ' ' + 'комнаты для' + ' ' + obj.offer.guests + ' ' + 'гостей';
+  mapCard.querySelector('.popup__text--time').innerText = 'Заезд после' + ' ' + obj.offer.checkin + ',' + 'выезд до' + ' ' + obj.offer.checkout;
 
   return mapCard;
 };
 
-mapCard.insertBefore(getMapCard(posters[0]), '.map__filters-container');
+map.insertBefore(getMapCard(posters[0]), filterContainer);
